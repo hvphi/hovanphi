274a2ac3cf5ed69230a585e8400a498364e10e4f
hvphi <50516445+hvphi@users.noreply.github.com>
2019-07-09 11:15:57 +0700

    Thay doi index

--- Upload.php
+++ Upload.php
@@ -1,29 +1,44 @@
 <?php
-   if(isset($_FILES['image'])){
-      $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size = $_FILES['image']['size'];
-      $file_tmp = $_FILES['image']['tmp_name'];
-      $file_type = $_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-      
-      $extensions= array("jpeg","jpg","png");
-      
-      if(in_array($file_ext,$extensions)=== false){
-         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-      }
-      
-      if($file_size > 2097152) {
-         $errors[]='File size must be excately 2 MB';
-      }
-      
-      if(empty($errors)==true) {
-         move_uploaded_file($file_tmp,"assets/".$file_name);
-         echo "Success";
-      }else{
-         print_r($errors);
-      }
-   }
+define ('MAX_FILE_SIZE', 1000000);
+
+$permitted = array('image/gif', 'image/jpeg', 'image/png', 'image/pjpeg', 'text/plain'); //Set array of permittet filetypes
+$error = true; //Define an error boolean variable
+$filetype = ""; //Just define it empty.
+
+foreach( $permitted as $key => $value ) //Run through all permitted filetypes
+{
+  if( $_FILES['file']['type'] == $value ) //If this filetype is actually permitted
+    {
+        $error = false; //Yay! We can go through
+        $filetype = explode("/",$_FILES['file']['type']); //Save the filetype and explode it into an array at /
+        $filetype = $filetype[0]; //Take the first part. Image/text etc and stomp it into the filetype variable
+    }
+}
+
+if  ($_FILES['file']['size'] > 0 && $_FILES['file']['size'] <= MAX_FILE_SIZE) //Check for the size
+{
+    if( $error == false ) //If the file is permitted
+    {
+        move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]); //Move the file from the temporary position till a new one.
+              if( $filetype == "image" ) //If the filetype is image, show it!
+              {
+                echo '<img src="upload/'.$_FILES["file"]["name"].'">';
+              }
+              elseif($filetype == "text") //If its text, print it.
+              {
+                echo nl2br( file_get_contents("upload/".$_FILES["file"]["name"]) );
+              }
+
+    }
+    else
+    {
+        echo "Not permitted filetype.";
+    }
+}
+else
+{
+  echo "File is too large.";
+}
 ?>
 <html>
    <body>
