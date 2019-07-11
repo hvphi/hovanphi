a786f8b9b20d7fbc7183308a057f41147e23a139
hvphi <hvphivh@gmail.com>
2019-07-11 16:16:36 +0700

    Upload files

--- Upload.php
+++ Upload.php
@@ -1,53 +1,6 @@
-<?php
-// upload files to s3 from Heroku using PHP SDK v3
-// requires config vars: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_BUCKET
-require('./../vendor/autoload.php');
-use Aws\S3\S3Client;
-use Aws\S3\Exception\S3Exception;
-?>
 <html>
     <head><meta charset="UTF-8"></head>
     <body>
-        <h1>S3 upload example</h1>
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) 
-    {
-	
-	// try upload
-	
-	// for now, use existing name
-	$uploadfolder = 'somefolder';
-	$uploadname = $_FILES['userfile']['name'];
-	
-	// set up s3
-	$bucket = getenv('S3_BUCKET');
-	$keyname = $uploadfolder.'/'.$uploadname;
-	$s3 = S3Client::factory([
-		'version' => '2006-03-01',
-		'region' => 'eu-west-1'
-	]);
-	
-	// try
-	try {
-	    // Upload data.
-	    $result = $s3->putObject(array(
-	        'Bucket' => $bucket,
-	        'Key'    => $keyname,
-	        'Body'   => fopen($_FILES['userfile']['tmp_name'], 'rb'),
-	        'ACL'    => 'public-read'
-	    ));
-	
-	    // Print the URL to the object.
-	    
-	    // show image
-	    echo('<p><img src="'.$result['ObjectURL'].'" /></p>');
-	    
-	} catch (S3Exception $e) {
-	    echo $e->getMessage() . "\n";
-	}	
-}
- ?>
-
         <h1>S3 upload example</h1>
         <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
             <input name="userfile" type="file"><input type="submit" value="Upload">
