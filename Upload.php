9674163bf92ce26e4113ff950b9e55c803d9c509
hvphi <hvphivh@gmail.com>
2019-07-11 13:32:39 +0700

    Upload files

--- Upload.php
+++ Upload.php
@@ -1,59 +1,30 @@
 <?php
-define ('MAX_FILE_SIZE', 10000000);
-
-$permitted = array('image/gif', 'image/jpeg', 'image/png', 'image/pjpeg', 'text/plain'); //Set array of permittet filetypes
-$error = true; //Define an error boolean variable
-$filetype = ""; //Just define it empty.
-
-foreach( $permitted as $key => $value ) //Run through all permitted filetypes
-{
-  if( $_FILES['file']['type'] == $value ) //If this filetype is actually permitted
-    {
-        $error = false; //Yay! We can go through
-        $filetype = explode("/",$_FILES['file']['type']); //Save the filetype and explode it into an array at /
-        $filetype = $filetype[0]; //Take the first part. Image/text etc and stomp it into the filetype variable
-    }
-}
-
-if  ($_FILES['file']['size'] > 0 && $_FILES['file']['size'] <= MAX_FILE_SIZE) //Check for the size
-{
-    if( $error == false ) //If the file is permitted
-    {
-        move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]); //Move the file from the temporary position till a new one.
-              if( $filetype == "image" ) //If the filetype is image, show it!
-              {
-                echo '<img src="upload/'.$_FILES["file"]["name"].'">';
-              }
-              elseif($filetype == "text") //If its text, print it.
-              {
-                echo nl2br( file_get_contents("upload/".$_FILES["file"]["name"]) );
-              }
-
-    }
-    else
-    {
-        echo "Not permitted filetype.";
-    }
-}
-else
-{
-  echo "File is too large.";
-}
+require('vendor/autoload.php');
+// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+$s3 = new Aws\S3\S3Client([
+    'version'  => '2006-03-01',
+    'region'   => 'us-east-1',
+]);
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
 <html>
-   <body>
-      
-      <form action = "" method = "POST" enctype = "multipart/form-data">
-         <input type = "file" name = "image" />
-         <input type = "submit"/>
-			
-         <ul>
-            <li>Sent file: <?php echo $_FILES['image']['name'];  ?>
-            <li>File size: <?php echo $_FILES['image']['size'];  ?>
-            <li>File type: <?php echo $_FILES['image']['type'] ?>
-         </ul>
-			
-      </form>
-      
-   </body>
+    <head><meta charset="UTF-8"></head>
+    <body>
+        <h1>S3 upload example</h1>
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    // FIXME: add more validation, e.g. using ext/fileinfo
+    try {
+        // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
+        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+?>
+        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
+<?php } catch(Exception $e) { ?>
+        <p>Upload error :(</p>
+<?php } } ?>
+        <h2>Upload a file</h2>
+        <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+            <input name="userfile" type="file"><input type="submit" value="Upload">
+        </form>
+    </body>
 </html>
