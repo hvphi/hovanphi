a2800069c623849f5e28f857dd56ed5a7ec33c6d
hvphi <hvphivh@gmail.com>
2019-07-11 16:14:40 +0700

    Upload files

--- Upload.php
+++ Upload.php
@@ -4,6 +4,12 @@
 require('./../vendor/autoload.php');
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
+?>
+<html>
+    <head><meta charset="UTF-8"></head>
+    <body>
+        <h1>S3 upload example</h1>
+<?php
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) 
     {
 	
@@ -41,9 +47,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES
 	}	
 }
  ?>
-<html>
-    <head><meta charset="UTF-8"></head>
-    <body>
+
         <h1>S3 upload example</h1>
         <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
             <input name="userfile" type="file"><input type="submit" value="Upload">
